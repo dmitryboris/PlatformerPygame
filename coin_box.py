6730242c551ea27ea71d02cb8bbc72f75d6f808e
Dima <dmitrij_bo05@mail.ru>
2022-11-03 12:01:47 +0500

    ыы

--- coin_box.py
+++ coin_box.py
@@ -3,7 +3,7 @@ import pygame
 
 class CoinBox(pygame.sprite.Sprite):
     def __init__(self, x, y, tile_width, tile_height, coin_box_im, *groups):
-        super().__init__(groups)
+        super().__init__(*groups)
         self.image = coin_box_im
         self.rect = self.image.get_rect().move(tile_width * x,
                                                tile_height * y)
